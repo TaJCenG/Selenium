7da64351c2c450d4dd9ae19f621109955b04aeb3
Tajceng <Tajusyed765@gmail.com>
2025-12-22 16:01:29 +0530

    suite1 for parallel

---
+++ src/test/java/suite1/TestC1.java
@@ -0,0 +1,14 @@
+package suite1;
+
+import org.testng.annotations.Test;
+
+public class TestC1 {
+	 @Test
+	  public void testC1() throws Exception 
+	  {
+		  System.out.println("Starting TestC1");
+		  Thread.sleep(2000);
+		  System.out.println("Ending TestC1");
+	  }
+
+}
