5194ca6b50e2bdb6923b645a928f76252248d7c1
Tajceng <Tajusyed765@gmail.com>
2025-12-22 16:02:05 +0530

    suite2 for parallel

---
+++ src/test/java/suite2/TestC2.java
@@ -0,0 +1,15 @@
+package suite2;
+
+import org.testng.annotations.Test;
+
+public class TestC2 {
+	
+	 @Test
+	  public void testC2() throws Exception 
+	  {
+		  System.out.println("Starting TestC2");
+		  Thread.sleep(2000);
+		  System.out.println("Ending TestC2");
+	  }
+
+}
